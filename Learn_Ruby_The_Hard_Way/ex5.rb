80ec89f1f354195e43cea3f646e762bc4552f46d
metaQ <qiutianchen@126.com>
2013-03-22 01:29:11 -0700

    some new exercises

---
+++ Learn_Ruby_The_Hard_Way/ex5.rb
@@ -0,0 +1,16 @@
+my_name = 'metaQ'
+my_age = 26 #not a lie 
+my_height = 178 #cm
+my_weight = 70 #kg
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+puts "Let's talk about %s." % my_name
+puts "He's %d cm tall." % my_height
+puts "He's %d kg heavy." % my_weight
+puts "Actually that's not too heavy."
+puts "He's got %s eyes and %s hair." % [my_eyes, my_hair]
+puts "His teeth are usually %s depending on the coffee." % my_teeth
+
+puts "If I add %d, %d, and %d I get %d." %[my_age, my_height, my_weight, my_age + my_height + my_weight]
